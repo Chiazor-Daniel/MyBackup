8f60a932662158197371066288a896eba4c43ddf
Chiazor-Daniel <chiazordaniel317@gmail.com>
2024-09-03 12:02:59 +0100

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import WheelOfFortune from './components/wheel';
 import NavBar from './components/navbar';
 import Footer from './components/footer';
+import CountryCodeForm from './components/country';
 
 function App() {
   const [showWheel, setShowWheel] = useState(false);
@@ -10,6 +11,11 @@ function App() {
   const toggleWheel = () => {
     setShowWheel(!showWheel);
   };
+  useEffect(()=> {
+    setTimeout(()=>{
+      setShowWheel(true)
+    }, 1000)
+  }, [])
   return (
     <div className="bg-black min-h-screen text-white">
       <NavBar />
@@ -86,15 +92,8 @@ function App() {
       {showWheel && (
         <div className="fixed inset-0 bg-black bg-opacity-50 backdrop-blur-sm flex justify-center items-center z-50">
           <div className=" p-8 rounded-lg relative">
-            <button 
-              className="absolute top-2 right-2 text-gray-400 hover:text-white"
-              onClick={toggleWheel}
-            >
-              <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
-              </svg>
-            </button>
-            <WheelOfFortune />
+          
+            <WheelOfFortune showWheel={showWheel}/>
           </div>
         </div>
       )}
--- src/components/wheel.jsx
+++ src/components/wheel.jsx
@@ -1,22 +1,42 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useEffect, useRef, ReactDOM } from 'react';
 import Swal from 'sweetalert2';
+import axios from 'axios';
+import ReactCountryFlag from 'react-country-flag';
+import CountryCodeForm from './country';
+import { RiseLoader } from 'react-spinners';
+import { useNavigate } from 'react-router-dom';
+
 
 const WheelOfFortune = () => {
   const [display, setDisplay] = useState('-');
   const [isSpinning, setIsSpinning] = useState(false);
+  const [userCountry, setUserCountry] = useState('');
   const wheelRef = useRef(null);
   const startButtonRef = useRef(null);
+  const navigate = useNavigate();
 
   const symbolSegments = {
-    1: "$10",
-    2: "$5",
-    3: "$1000",
-    4: "$500",
-    5: "$0",
-    6: "$250",
-    7: "$100",
-    8: "Mystery Box",
+    1: "$2000",
+    2: "$3000",
+    3: "$10000",
+    4: "$5000",
+    5: "$1500",
+    6: "$4500",
+    7: "$3500",
+    8: "2000",
   };
+  const fetchUserCountry = async () => {
+    try {
+      const response = await axios.get('https://ipapi.co/json/');
+      setUserCountry(response.data.country_name);
+    } catch (error) {
+      console.error('Error fetching user country:', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchUserCountry();
+  }, []);
 
   const spin = () => {
     if (isSpinning) return;
@@ -37,9 +57,11 @@ const WheelOfFortune = () => {
       wheel.style.transition = 'none';
       const actualDeg = deg % 360;
       wheel.style.transform = `rotate(${actualDeg}deg)`;
+      Swal.close();
       handleWin(actualDeg);
     }, 10000);
   };
+
 
   const handleWin = (actualDeg) => {
     const zoneSize = 45;
@@ -47,99 +69,46 @@ const WheelOfFortune = () => {
     const prize = symbolSegments[winningSymbolNr];
     setDisplay(prize);
 
-    if (prize === "Mystery Box") {
-      Swal.fire({
-        title: 'Mystery Box!',
-        text: 'You won a Mystery Box! Would you like to open it?',
-        icon: 'question',
-        showCancelButton: true,
-        confirmButtonColor: '#4CAF50',
-        cancelButtonColor: '#f44336',
-        confirmButtonText: 'Open Mystery Box',
-        background: '#2C3E50',
-        color: '#ECF0F1',
-        iconColor: '#F39C12',
-        customClass: {
-          popup: 'custom-popup',
-          title: 'custom-title',
-          content: 'custom-content',
-          confirmButton: 'custom-button',
-          cancelButton: 'custom-button'
-        }
-      }).then((result) => {
-        if (result.isConfirmed) {
-          Swal.fire({
-            title: 'Congratulations!',
-            text: 'You opened the Mystery Box!',
-            icon: 'success',
-            confirmButtonText: 'Claim Prize',
-            background: '#2C3E50',
-            color: '#ECF0F1',
-            iconColor: '#2ECC71',
-            customClass: {
-              popup: 'custom-popup',
-              title: 'custom-title',
-              content: 'custom-content',
-              confirmButton: 'custom-button'
-            }
-          }).then((result) => {
-            if (result.isConfirmed) {
-              showClaimForm();
-            }
-          });
-        }
-      });
-    } else if (prize !== "$0") {
-      Swal.fire({
-        title: 'Congratulations!',
-        text: `You won ${prize}!`,
-        icon: 'success',
-        confirmButtonText: 'Claim Prize',
-        background: '#2C3E50',
-        color: '#ECF0F1',
-        iconColor: '#2ECC71',
-        customClass: {
-          popup: 'custom-popup',
-          title: 'custom-title',
-          content: 'custom-content',
-          confirmButton: 'custom-button'
-        }
-      }).then((result) => {
-        if (result.isConfirmed) {
-          showClaimForm();
-        }
-      });
-    } else {
-      Swal.fire({
-        title: 'Oops!',
-        text: 'You landed on $0. Spin again for another chance!',
-        icon: 'info',
-        confirmButtonText: 'Spin Again',
-        background: '#2C3E50',
-        color: '#ECF0F1',
-        iconColor: '#3498DB',
-        customClass: {
-          popup: 'custom-popup',
-          title: 'custom-title',
-          content: 'custom-content',
-          confirmButton: 'custom-button'
-        }
-      }).then((result) => {
-        if (result.isConfirmed) {
-          spin(); // Automatically spin again if the user clicks the button
-        }
-      });
-    }
+    Swal.fire({
+      title: 'Congratulations!',
+      text: `You won ${prize}!`,
+      icon: 'success',
+      confirmButtonText: 'Claim Prize',
+      background: '#2C3E50',
+      color: '#ECF0F1',
+      iconColor: '#2ECC71',
+      customClass: {
+        popup: 'custom-popup',
+        title: 'custom-title',
+        content: 'custom-content',
+        confirmButton: 'custom-button'
+      }
+    }).then((result) => {
+      if (result.isConfirmed) {
+        showClaimForm(prize);
+      }
+    });
   };
 
-  const showClaimForm = () => {
+
+  const showClaimForm = (prize) => {
     Swal.fire({
       title: 'Claim Your Prize',
-      html:
-        '<input id="name" class="swal2-input custom-input" placeholder="Full Name">' +
-        '<input id="email" class="swal2-input custom-input" placeholder="Email">' +
-        '<input id="phone" class="swal2-input custom-input" placeholder="Phone Number">' +
-        '<textarea id="address" class="swal2-textarea custom-textarea" placeholder="Mailing Address"></textarea>',
+      html: `
+        <div class="claim-form">
+          <div class="form-row">
+            <input id="first_name" class="swal2-input custom-input" placeholder="First Name">
+            <input id="last_name" class="swal2-input custom-input" placeholder="Last Name">
+          </div>
+          <div class="form-row">
+            <input id="email" class="swal2-input custom-input" placeholder="Email">
+            <input id="phone_number" class="swal2-input custom-input" placeholder="Phone Number">
+          </div>
+          <div class="form-row">
+            <input id="country" class="swal2-input custom-input" placeholder="Country" value="${userCountry}">
+          </div>
+        </div>
+      `,
       focusConfirm: false,
       confirmButtonText: 'Submit',
       showCancelButton: true,
@@ -155,19 +124,56 @@ const WheelOfFortune = () => {
       },
       preConfirm: () => {
         return {
-          name: document.getElementById('name').value,
+          first_name: document.getElementById('first_name').value,
+          last_name: document.getElementById('last_name').value,
           email: document.getElementById('email').value,
-          phone: document.getElementById('phone').value,
-          address: document.getElementById('address').value
-        }
+          phone_number: document.getElementById('phone_number').value,
+          country: document.getElementById('country').value,
+          bonus_amount: parseFloat(prize.replace('$', '')),
+        };
       }
     }).then((result) => {
       if (result.isConfirmed) {
-        // Here you would typically send the form data to your server
-        console.log(result.value);
+        submitClaimForm(result.value);
+      }
+    });
+  };
+
+
+
+  const submitClaimForm = async (formData) => {
+    Swal.fire({
+      title: 'Submitting...',
+      html: 'Please wait while we process your claim.',
+      allowOutsideClick: false,
+      showConfirmButton: false,
+      willOpen: () => {
+        Swal.showLoading();
+      },
+      background: '#2C3E50',
+      color: '#ECF0F1',
+    });
+
+    try {
+      const response = await axios.post(
+        `https://finno-api.finnovent.com/general-route/create-new-user-from-ads?first_name=${formData.first_name}&last_name=${formData.last_name}&email=${formData.email}&phone_number=${formData.phone_number}&country=${formData.country}&bonus_amount=${formData.bonus_amount}`,
+        null,
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            'x-token': 'hi8qX8qLpJmCYQX5eL7Ifz47CpOtxf2EpyxxGh7MItghqs34mo'
+          }
+        }
+      );
+
+      const prizeToken = response?.data?.message;
+
+      if (response.data.status === 'success') {
+        document.cookie = `prizeToken=${prizeToken}; path=/; domain=localhost; secure; samesite=lax;`;
+
         Swal.fire({
-          title: 'Thank You!',
-          text: 'Your prize claim has been submitted.',
+          title: 'Success!',
+          text: 'Your prize claim has been submitted successfully.',
           icon: 'success',
           background: '#2C3E50',
           color: '#ECF0F1',
@@ -178,16 +184,64 @@ const WheelOfFortune = () => {
             content: 'custom-content',
             confirmButton: 'custom-button'
           }
+        }).then((result) => {
+          if (result.isConfirmed) {
+            navigate(`localhost:5173`);
+          }
+        });
+      } else {
+        throw new Error('Submission failed');
+      }
+    } catch (error) {
+      const errorMessage = error.response?.data?.message;
+      if (errorMessage === "Unable to create user") {
+        Swal.fire({
+          title: 'Already Claimed',
+          text: 'It looks like you have already claimed the prize. Proceed to the dashboard to view your rewards.',
+          icon: 'info',
+          background: '#2C3E50',
+          color: '#ECF0F1',
+          iconColor: '#F39C12',
+          showCancelButton: true,
+          confirmButtonText: 'Proceed to Dashboard',
+          cancelButtonText: 'Cancel',
+          customClass: {
+            popup: 'custom-popup',
+            title: 'custom-title',
+            content: 'custom-content',
+            confirmButton: 'custom-button',
+            cancelButton: 'custom-button'
+          }
+        }).then((result) => {
+          if (result.isConfirmed) {
+            window.location.href = '/dashboard';
+          }
+        });
+      } else {
+        Swal.fire({
+          title: 'Error',
+          text: 'There was an error submitting your claim. Please try again.',
+          icon: 'error',
+          background: '#2C3E50',
+          color: '#ECF0F1',
+          iconColor: '#E74C3C',
+          customClass: {
+            popup: 'custom-popup',
+            title: 'custom-title',
+            content: 'custom-content',
+            confirmButton: 'custom-button'
+          }
         });
       }
-    });
+    }
   };
+
 
   return (
     <div className="wheel-of-fortune">
       <div id="app">
         <img className="marker" src="marker.png" alt="Marker" />
-        <img className="wheel" src="spin.png" alt="Wheel" ref={wheelRef} />
+        <img className="wheel" src="wee.png" alt="Wheel" ref={wheelRef} />
         <img
           className="button"
           src="button.png"
@@ -197,51 +251,77 @@ const WheelOfFortune = () => {
           style={{ pointerEvents: isSpinning ? 'none' : 'auto' }}
         />
       </div>
+
       <style jsx>{`
-        .wheel-of-fortune {
-          font-family: Arial, Helvetica, sans-serif;
-        }
-        #app {
-          width: 400px;
-          height: 400px;
-          margin: 0 auto;
-          position: relative;
-        }
-        .marker {
-          position: absolute;
-          width: 60px;
-          left: 172px;
-          top: -20px;
-          z-index: 2;
-        }
-        .wheel {
-          width: 100%;
-          height: 100%;
-        }
-        .button {
-          display: block;
-          width: 250px;
-          margin: 40px auto;
-          cursor: pointer;
-        }
-        .button:hover {
-          opacity: 0.8;
-        }
-        .blur {
-          animation: blur 10s;
-        }
-        @keyframes blur {
-          0% {
-            filter: blur(1.5px);
-          }
-          80% {
-            filter: blur(1.5px);
-          }
-          100% {
-            filter: blur(0px);
-          }
-        }
-      `}</style>
+  .wheel-of-fortune {
+    font-family: Arial, Helvetica, sans-serif;
+  }
+  #app {
+    width: 400px;
+    height: 400px;
+    margin: 0 auto;
+    position: relative;
+    transform: scale(1);
+  }
+  .marker {
+    position: absolute;
+    width: 60px;
+    left: 172px;
+    top: -20px;
+    z-index: 2;
+  }
+  .wheel {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+  .button {
+    display: block;
+    width: 250px;
+    z-index: 999;
+    margin: 10px auto;
+    cursor: pointer;
+  }
+  .button:hover {
+    opacity: 0.8;
+  }
+  .blur {
+    animation: blur 10s;
+  }
+  @keyframes blur {
+    0% {
+      filter: blur(1.5px);
+    }
+    80% {
+      filter: blur(1.5px);
+    }
+    100% {
+      filter: blur(0px);
+    }
+  }
+  .user-country {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-top: 20px;
+  }
+  .user-country span {
+    margin-left: 10px;
+  }
+
+  /* Media queries for desktop and tablet view */
+  @media (min-width: 768px) { /* Tablet and up */
+    #app {
+      transform: scale(1.4);
+    }
+  }
+  @media (min-width: 1024px) { /* Desktop and up */
+    #app {
+      transform: scale(1.4);
+    }
+  }
+`}</style>
+
       <style jsx global>{`
         .custom-popup {
           border-radius: 15px;
